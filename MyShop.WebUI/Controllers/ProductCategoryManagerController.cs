a00935bc122f891939c2a32c3ced18f86f9cafc8
JoanFowler <joan.fowler@cch.co.nz>
2022-12-30 12:18:05 +1300

    Set up dependency injection using MIcrosoft Unity

--- MyShop.WebUI/Controllers/ProductCategoryManagerController.cs
+++ MyShop.WebUI/Controllers/ProductCategoryManagerController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MyShop.Core.Contracts;
 using MyShop.Core.Models;
 using MyShop.DataAccess.InMemory;
 
@@ -11,10 +12,10 @@ namespace MyShop.WebUI.Controllers
 {
     public class ProductCategoryManagerController : Controller
     {
-        InMemoryRepository<ProductCategory> context;
-        public ProductCategoryManagerController()
+        IRepository<ProductCategory> context;
+        public ProductCategoryManagerController(IRepository<ProductCategory> productContext)
         {
-            context = new InMemoryRepository<ProductCategory>();
+            context = productContext;
         }
 
         // GET: ProductManager
