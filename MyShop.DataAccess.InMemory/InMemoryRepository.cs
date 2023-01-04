5035fcffe966559d7058bb7c8fa937a731ca7cc3
JoanFowler <joan.fowler@cch.co.nz>
2023-01-05 11:35:24 +1300

    Implemented Git repository

--- MyShop.DataAccess.InMemory/InMemoryRepository.cs
+++ MyShop.DataAccess.InMemory/InMemoryRepository.cs
@@ -67,9 +67,9 @@ namespace MyShop.DataAccess.InMemory
             return items.AsQueryable();
         }
 
-        public void Delete(T t)
+        public void Delete(string Id)
         {
-            T tToDelete = items.Find(i => i.Id == t.Id);
+            T tToDelete = items.Find(i => i.Id == Id);
             if (tToDelete != null)
             {
                 items.Remove(tToDelete);
--- MyShop.WebUI/Controllers/ProductManagerController.cs
+++ MyShop.WebUI/Controllers/ProductManagerController.cs
@@ -127,7 +127,7 @@ namespace MyShop.WebUI.Controllers
             }
             else
             {
-                context.Delete(productToDelete);
+                context.Delete(Id);
                 context.Commit();
                 return RedirectToAction("Index");
             }
